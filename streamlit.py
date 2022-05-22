62422737e92456a4e73db6118c332905e0440d61
ME3ZA <101148207+ME3ZA@users.noreply.github.com>
2022-05-22 11:46:28 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import tensorflow as tf
 import numpy as np
 from PIL import Image
-from .cv2 import *
+import cv2
 
 model_path=r'C:\Users\user\Desktop\Graduation\chest_model_balanced.h5'
 
